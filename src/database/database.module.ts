36afd09a829b9f1fc90a8e0988eb7a868985f74d
Pagalavan7 <pagalavanvetri@gmail.com>
2024-09-28 17:10:12 +0530

    db connect complete

---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_SERVER,
+      port: 1433,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      options: {
+        enableArithAbort: true,
+        encrypt: true,
+        trustServerCertificate: true,
+      },
+      autoLoadEntities: true,
+      synchronize: true,
+      // synchronize: process.env.DB_SYNCHRONIZE,
+    }),
+  ],
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,3 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
-
-// const dataSource = new DataSource({
-//   type: "mssql",
-//   host: process.env.DB_SERVER,
-//   port: 1433,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   options: {
-//     enableArithAbort: true,
-//     encrypt: true,
-//     trustServerCertificate: true,
-//   },
-//   entities: [Client],
-//   synchronize: true,
-// });
-
-// dataSource
-//   .initialize()
-//   .then(() => console.log("DB connection successful"))
-//   .catch((err) => {
-//     console.log(err.message || err);
-//   });
